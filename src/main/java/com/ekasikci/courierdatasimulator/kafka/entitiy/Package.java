cc896b1da5a808e9ecaa18b3db7989a76584d0dd
ekasikci <ekasikci2000@gmail.com>
2025-12-06 09:56:38 +0300

    add Package entity

---
+++ src/main/java/com/ekasikci/courierdatasimulator/kafka/entitiy/Package.java
@@ -0,0 +1,97 @@
+package com.ekasikci.courierdatasimulator.kafka.entitiy;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "packages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Package {
+
+    @Id
+    private Long id;
+
+    @Column(name = "arrival_for_delivery_at")
+    private LocalDateTime arrivalForDeliveryAt;
+
+    @Column(name = "arrival_for_pickup_at")
+    private LocalDateTime arrivalForPickupAt;
+
+    @Column(name = "cancel_reason")
+    private String cancelReason;
+
+    // Could be Boolean, but Integer is more common and safe
+    @Column(name = "cancelled")
+    private Integer cancelled;
+
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "in_delivery_at")
+    private LocalDateTime inDeliveryAt;
+
+    @Column(name = "last_updated_at")
+    private LocalDateTime lastUpdatedAt;
+
+    @Column(name = "eta")
+    private Integer eta;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "store_id")
+    private Long storeId;
+
+    @Column(name = "origin_address_id")
+    private Long originAddressId;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "waiting_for_assignment_at")
+    private LocalDateTime waitingForAssignmentAt;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "collected")
+    private Integer collected;
+
+    @Column(name = "collected_at")
+    private LocalDateTime collectedAt;
+
+    @Column(name = "cancelled_at")
+    private LocalDateTime cancelledAt;
+
+    @Column(name = "picked_up_at")
+    private LocalDateTime pickedUpAt;
+
+    @Column(name = "reassigned")
+    private String reassigned;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "delivery_date")
+    private String deliveryDate;
+
+    public boolean isCancelled() {
+        return cancelled != null && cancelled == 1;
+    }
+
+    public boolean isCompleted() {
+        return "COMPLETED".equalsIgnoreCase(status);
+    }
+}
