3c80cf45e252f47dcc62881357b6283ada94718c
ekasikci <ekasikci2000@gmail.com>
2025-12-06 09:30:10 +0300

    first commit

---
+++ src/main/java/com/ekasikci/courierdatasimulator/CourierDataSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.ekasikci.courierdatasimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierDataSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierDataSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekasikci/courierdatasimulator/CourierDataSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekasikci.courierdatasimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierDataSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
