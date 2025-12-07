d7a8b0f3f581014c7fabfb19ee120c0638529816
ekasikci <ekasikci2000@gmail.com>
2025-12-07 22:00:46 +0300

    add unimplemented ingestion strategy files

---
+++ src/main/java/com/ekasikci/courierdatasimulator/kafka/strategy/BatchIngestionStrategy.java
@@ -0,0 +1,40 @@
+package com.ekasikci.courierdatasimulator.kafka.strategy;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.ekasikci.courierdatasimulator.kafka.config.IngestionStrategyConfig;
+import com.ekasikci.courierdatasimulator.kafka.entitiy.Package;
+import com.ekasikci.courierdatasimulator.kafka.service.KafkaProducerService;
+import com.ekasikci.courierdatasimulator.kafka.service.PackageService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BatchIngestionStrategy implements IngestionStrategy {
+
+    private final PackageService packageService;
+    private final KafkaProducerService kafkaProducerService;
+    private final IngestionStrategyConfig config;
+
+    @Override
+    public void ingest(List<Package> packages) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ingest'");
+    }
+
+    @Override
+    public String getStrategyName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getStrategyName'");
+    }
+
+    @Override
+    public boolean isActive() {
+        return "BATCH".equalsIgnoreCase(config.getStrategy());
+    }
+}
